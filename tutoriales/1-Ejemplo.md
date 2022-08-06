3d069e4db9aaff79a47d68d7b9c567c814b35b6d
Carlos Cuxim <carlos.cuxim@gmail.com>
2022-08-05 22:25:00 -0500

    Agregado mecanismo para carga de los tutoriales

---
+++ tutoriales/1-Ejemplo.md
@@ -0,0 +1,10 @@
+# Ejemplo
+
+Este es un ejemplo de un tipo de yo ke se
+
+$$ Hola $$
+
+1. Banana
+2. Plátano
+3. Guineo
+
---
+++ tutoriales/script.js
@@ -0,0 +1,7 @@
+var S = location.search.substring(1)
+
+var Ob = document.getElementById("temporal")
+
+Ob.data = S + ".md"
+
+console.log(Ob)
